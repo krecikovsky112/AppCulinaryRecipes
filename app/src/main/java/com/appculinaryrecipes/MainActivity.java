b8f7ee6a3a29e9f4f3e3256232819d929bd9a646
michalkedzia <kedziamichal0@gmail.com>
2021-10-17 21:29:07 +0200

    Dodanie możliwości rejestrowania/logowania za pomocą konta google.

--- app/src/main/java/com/appculinaryrecipes/MainActivity.java
+++ app/src/main/java/com/appculinaryrecipes/MainActivity.java
@@ -1,14 +1,41 @@
 package com.appculinaryrecipes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.appculinaryrecipes.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        activityMainBinding.logout.setOnClickListener(v -> {
+            firebaseAuth.signOut();
+            checkUser();
+        });
+
     }
+
+    private void checkUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, AuthActivity.class));
+        } else {
+            activityMainBinding.testText.setText(firebaseUser.getEmail());
+        }
+    }
+
 }
