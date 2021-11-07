0ce7ab959a2a27cebc68f6d5097d7433ce186cb8
klesniewski50 <g3zv1ag8dx1ms2@gmail.com>
2021-11-07 20:22:40 +0100

    dziłające przełączanie się między fragmentami

--- app/src/main/java/com/appculinaryrecipes/MainActivity.java
+++ app/src/main/java/com/appculinaryrecipes/MainActivity.java
@@ -2,20 +2,29 @@ package com.appculinaryrecipes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.adapters.AdapterViewBindingAdapter;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.appculinaryrecipes.databinding.ActivityMainBinding;
+import com.appculinaryrecipes.databinding.FragmentHomeBinding;
+import com.appculinaryrecipes.generated.callback.OnClickListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private ActivityMainBinding activityMainBinding;
+    private ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,25 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragmentHomeContainer, fragment);
         fragmentTransaction.commit();
+        chipNavigationBar = findViewById(R.id.navigationBar);
+        chipNavigationBar.setItemSelected(R.id.home,true);
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.creator:
+                        fragment = new RecipeFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentHomeContainer,
+                                fragment).commit();
+            }
+        });
 
     }
 
