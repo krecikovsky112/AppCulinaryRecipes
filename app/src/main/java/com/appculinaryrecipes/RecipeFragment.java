297b0ea6c0917df92a219fe19c82ed561408e2ae
krecikovsky112 <krecikovsky112@gmail.com>
2021-11-14 19:53:11 +0100

    Add button to add recipe to favorites and generate shopping list

--- app/src/main/java/com/appculinaryrecipes/RecipeFragment.java
+++ app/src/main/java/com/appculinaryrecipes/RecipeFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -27,7 +28,6 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 public class RecipeFragment extends Fragment {
-
     private static final String ARG_PARAM1 = "name";
     private static final String ARG_PARAM2 = "image";
     private static final String ARG_PARAM3 = "id";
@@ -38,6 +38,7 @@ public class RecipeFragment extends Fragment {
     private FragmentRecipeBinding fragmentRecipeBinding;
     private ArrayList<String> ingriedients;
     private ArrayList<String> measures;
+    private boolean flagButtonLike = false;
 
     public RecipeFragment() {
         // Required empty public constructor
@@ -73,27 +74,21 @@ public class RecipeFragment extends Fragment {
         fragmentRecipeBinding.titleRecipe.setText(title);
         Picasso.get().load(imageURL).into(fragmentRecipeBinding.imageRecipe);
 
-        getIngriedients();
+        getInfoRecipe();
 
         return view;
     }
 
 
     private void setIngriedientsInTextView() {
-        TextView title = new TextView(getActivity());
-        title.setTextSize(40);
-        title.setTextColor(Color.parseColor("#ae0216"));
-        title.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        setTitle("Ingredients");
         Typeface face = ResourcesCompat.getFont(getActivity(), R.font.dongle_regular);
-        title.setTypeface(face);
-        title.setText("Ingriedients");
-        fragmentRecipeBinding.container.addView(title);
         for (int i = 0; i < ingriedients.size(); i++) {
             String ingriedient = ingriedients.get(i);
             String measure = measures.get(i);
             TextView textView = new TextView(getActivity());
             textView.setTextSize(25);
-            textView.setPadding(100, 0, 0, 0);
+            textView.setPadding(0, 0, 0, 0);
             textView.setTextColor(Color.parseColor("#ae0216"));
             textView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
             textView.setTypeface(face);
@@ -102,7 +97,7 @@ public class RecipeFragment extends Fragment {
         }
     }
 
-    private void getIngriedients() {
+    private void getInfoRecipe() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference documentReference = db
                 .collection(DB_RECIPE_DOCUMENT_NAME).document(id);
@@ -115,6 +110,7 @@ public class RecipeFragment extends Fragment {
                 String instruction = document.getString("instructions");
                 setInstuctionTextView(instruction);
                 String urlVideo = document.getString("youtube");
+                assert urlVideo != null;
                 setYoutubePlayer(urlVideo);
             } else {
                 System.out.println("get failed with " + task.getException());
@@ -123,21 +119,27 @@ public class RecipeFragment extends Fragment {
     }
 
     private void setYoutubePlayer(String urlVideo) {
-        Typeface face = ResourcesCompat.getFont(getActivity(), R.font.dongle_regular);
-        TextView textView = new TextView(getActivity());
-        textView.setTextSize(40);
-        textView.setTextColor(Color.parseColor("#ae0216"));
-        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        textView.setTypeface(face);
-        textView.setText("Video");
-        fragmentRecipeBinding.container.addView(textView);
+        setTitle("Video");
 
         YouTubePlayerView youTubePlayerView = new YouTubePlayerView(getActivity());
-        youTubePlayerView.setPadding(100,0,100,20);
+        youTubePlayerView.setPadding(0,0,0,60);
         youTubePlayerView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        youTubePlayerView.setLayoutParams(new FrameLayout.LayoutParams(1400,800));
+        youTubePlayerView.setLayoutParams(new FrameLayout.LayoutParams(1300,1000));
         fragmentRecipeBinding.container.addView(youTubePlayerView);
 
+        String result = substringURLVideoToVideoId(urlVideo);
+        getLifecycle().addObserver(youTubePlayerView);
+        String finalResult = result;
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+                @Override
+                public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                    youTubePlayer.cueVideo(finalResult,0);
+                }
+            });
+    }
+
+    @Nullable
+    private String substringURLVideoToVideoId(String urlVideo) {
         assert urlVideo != null;
         char[] temp = urlVideo.toCharArray();
         String result = null;
@@ -145,35 +147,56 @@ public class RecipeFragment extends Fragment {
             if (temp[i] == '='){
                 result = urlVideo.substring(i+1, temp.length);
             }
-        getLifecycle().addObserver(youTubePlayerView);
-        String finalResult = result;
-        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-                @Override
-                public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                    youTubePlayer.cueVideo(finalResult,0);
-//                    youTubePlayer.pause();
-                }
-            });
+        return result;
     }
 
-    private void setInstuctionTextView(String instruction) {
+    private void setTitle(String name) {
         Typeface face = ResourcesCompat.getFont(getActivity(), R.font.dongle_regular);
         TextView textView = new TextView(getActivity());
         textView.setTextSize(40);
+        textView.setPadding(0,50,0,0);
         textView.setTextColor(Color.parseColor("#ae0216"));
         textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         textView.setTypeface(face);
-        textView.setText("Instruction");
+        textView.setText(name);
         fragmentRecipeBinding.container.addView(textView);
+    }
 
-        textView = new TextView(getActivity());
+    private void setInstuctionTextView(String instruction) {
+        setTitle("Instruction");
+        Typeface face = ResourcesCompat.getFont(getActivity(), R.font.dongle_regular);
+        TextView textView = new TextView(getActivity());
         textView.setTextSize(25);
-        textView.setPadding(100, 0, 100, 0);
+        textView.setPadding(0, 0, 0, 0);
         textView.setTextColor(Color.parseColor("#ae0216"));
         textView.setTextAlignment(View.TEXT_ALIGNMENT_GRAVITY);
         textView.setTypeface(face);
         textView.setText(instruction);
-//        textView.setBackgroundResource(R.drawable.background_instruction);
         fragmentRecipeBinding.container.addView(textView);
     }
+
+    //TODO: Trzeba dodać tutaj zapamiętywanie stanu czy dany przepis jest w ulubionych czy nie
+    public void onClickLike(){
+        if(!flagButtonLike)
+        {
+            fragmentRecipeBinding.favBtn.setBackgroundResource(R.drawable.ic_favorite_red_24);
+            flagButtonLike = true;
+        }
+        else{
+            fragmentRecipeBinding.favBtn.setBackgroundResource(R.drawable.ic_baseline_favorite_border_24);
+            flagButtonLike = false;
+        }
+    }
+
+    //TODO: Do zaimplementowanie przejście do generatora listy zakupów
+    public void onClickGenerateList(){
+
+    }
 }
+
+
+
+
+
+
+
